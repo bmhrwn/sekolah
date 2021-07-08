471446572a441dfdb6f208a72d44e367b2fa950f
bmhrwn <bmhrwn@gmail.com>
2021-07-09 00:32:36 +0700

    nis sama tanggal

--- application/views/home/data/pendaftaran.php
+++ application/views/home/data/pendaftaran.php
@@ -19,31 +19,34 @@
                     <h4>Silahkan isi halaman form pendaftaran sesuai ketentuan yang ada.</h4>
                 </center>
                 <div class="col-md-6">
-                    <div class="form-group row mt-4">
-                        <form action="<?= base_url() ?>pendaftaran/process_pendaftaran" method="post" enctype="multipart/form-data">
+                    <form action="<?= base_url() ?>pendaftaran/process_pendaftaran" method="post" enctype="multipart/form-data">
+                        <div class="form-group row mt-4">
                             <label for="" class="col-sm-3 col-form-label">NISN</label>
                             <div class="col-sm-9">
-                                <input type="text" value="" id="" required name="nis" class="form-control">
+                                <input type="number"autocomplete="off"autocorrect="false" value="" id="" required name="nis" onkeyup="checkNis(this)" class="form-control">
+                                <p style="color: red;
+                                "hidden="true"id="notif">Silahkan masukan nis yang benar ! (10 digit)</p>
                             </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="" class="col-sm-3 col-form-label">Nama Lengkap</label>
-                        <div class="col-sm-9">
-                            <input type="text" value="" id="" required name="full_name" class="form-control">
                         </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="" class="col-sm-3 col-form-label">Tanggal Lahir</label>
-                        <div class="col-sm-9">
-                            <input type="date" value="" id="tgl_lahir" required name="tgl_lahir" class="form-control">
+                        <div class="form-group row">
+                            <label for="" class="col-sm-3 col-form-label">Nama Lengkap</label>
+                            <div class="col-sm-9">
+                                <input type="text" value="" id="" required name="full_name" class="form-control">
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="" class="col-sm-3 col-form-label">Nama Orang Tua</label>
-                        <div class="col-sm-9">
-                            <input type="text" value="" id="" required name="nama_orangtua" class="form-control">
+                        <div class="form-group row">
+                            <label for="" class="col-sm-3 col-form-label">Tanggal Lahir</label>
+                            <div class="col-sm-9">
+                                <input type="date" value="" onchange="checkDate(this)" id="tgl_lahir" required name="tgl_lahir" class="form-control">
+                                <p id="notif_tanggal" hidden="true" style="color: red;">Mohon Maaf umur anda belum mencukup untuk melakukan pendaftaran mutasi</p>
+                            </div>
                         </div>
-                    </div>
+                        <div class="form-group row">
+                            <label for="" class="col-sm-3 col-form-label">Nama Orang Tua</label>
+                            <div class="col-sm-9">
+                                <input type="text" value="" id="" required name="nama_orangtua" class="form-control">
+                            </div>
+                        </div>
                 </div>
                 <div class="col-md-6">
                     <div class="form-group row">
@@ -133,7 +136,7 @@
                     <h4>Silahkan isi halaman form pendaftaran sesuai ketentuan yang ada.</h4>
                 </center>
                 <div class="col-md-6 mt-4">
-                <div class="form-group row">
+                    <div class="form-group row">
                         <label for="formFile" class="col-sm-3 col-form-label">Foto NISN</label>
                         <div class="col-sm-9">
                             <input class="form-control" type="file" required name="bukti_nis" id="formFile">
@@ -174,7 +177,7 @@
             <div class="d-grid gap-2 d-md-block">
                 <center>
                     <button class="btn btn-outline-danger" onClick="back(3)" type="button">Kembali</button>
-                    <button class="btn btn-primary" type="submit">Daftar</button>
+                    <button class="btn btn-primary" id="btn_daftar" type="submit">Daftar</button>
                 </center>
             </div>
         </div>
@@ -194,6 +197,49 @@
             document.getElementById('data_3').hidden = false;
         }
     }
+
+    function checkNis(field) {
+        let nis = field.value;
+        console.log(nis.length);
+        if(nis.length > 10 || nis.length < 10){
+            document.getElementById("notif").hidden = false;
+            $("#btn_daftar").attr('disabled','disabled');
+        }else{
+            document.getElementById("notif").hidden = true;
+            $("#btn_daftar").removeAttr('disabled');
+        }
+    }
+    
+    function checkDate(field){
+        let date = field.value;
+        let today = new Date();
+        let dd = String(today.getDate()).padStart(2,'0')
+        let mm = String(today.getMonth() + 1).padStart(2,'0');
+        let yy = String(today.getFullYear());
+        let fullDate = yy+'-'+mm+'-'+dd;
+        let arrayDate = getRange(date,fullDate);
+        let minimalUmur = 4015;  // 11 tahun
+        if(arrayDate.length <= 4015){
+            document.getElementById("notif_tanggal").hidden = false
+            $("#btn_daftar").attr('disabled','disabled');
+        }else{
+            $("#btn_daftar").removeAttr('disabled');
+              document.getElementById("notif_tanggal").hidden = true
+
+      }
+
+  }
+
+  function getRange(startDate,stopDate,steps = 1){
+      var dateArray = [];
+      let currentDate = new Date(startDate);
+      while(currentDate <= new Date(stopDate)){
+          dateArray.push(new Date(currentDate));
+          currentDate.setUTCDate(currentDate.getUTCDate() + steps);
+
+      }
+      return dateArray
+  }
 
     function back(page) {
         if (page == 3) {
